1a785cb71bfa1b4d74375d60c6c03202288956ea
jes <suarezliceajesus@gmail.com>
2024-10-18 22:32:41 -0600

    Nueva version!

--- src/Autolegal-Ags-frontend/src/pages/NewCar.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function NewCar() {
-  return (
-    <div>NewCar</div>
-  )
-}
--- src/Autolegal-Ags-frontend/src/pages/NewLicence.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function NewLicence() {
-  return (
-    <div>NewLicence</div>
-  )
-}
